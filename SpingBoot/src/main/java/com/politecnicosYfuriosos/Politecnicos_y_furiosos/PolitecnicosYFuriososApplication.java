6283bc220e16145728329fb223366542d8ca6174
scalulo <alumno26.scanio.lautaro@ipm.edu.ar>
2025-10-13 08:43:45 -0300

    subo el spring boot

---
+++ SpingBoot/src/main/java/com/politecnicosYfuriosos/Politecnicos_y_furiosos/PolitecnicosYFuriososApplication.java
@@ -0,0 +1,13 @@
+package com.politecnicosYfuriosos.Politecnicos_y_furiosos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolitecnicosYFuriososApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolitecnicosYFuriososApplication.class, args);
+	}
+
+}
---
+++ SpingBoot/src/test/java/com/politecnicosYfuriosos/Politecnicos_y_furiosos/PolitecnicosYFuriososApplicationTests.java
@@ -0,0 +1,13 @@
+package com.politecnicosYfuriosos.Politecnicos_y_furiosos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolitecnicosYFuriososApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
