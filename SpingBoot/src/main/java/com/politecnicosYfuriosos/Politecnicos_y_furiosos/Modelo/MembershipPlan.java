21a6c6a54345c39338dcd8391abdde05139c6ef7
scalulo <167205361+scalulo@users.noreply.github.com>
2025-10-30 10:00:34 -0300

    Delete SpingBoot directory

--- SpingBoot/src/main/java/com/politecnicosYfuriosos/Politecnicos_y_furiosos/Modelo/MembershipPlan.java
+++
@@ -1,7 +0,0 @@
-package com.politecnicosYfuriosos.Politecnicos_y_furiosos.Modelo;
-
-public enum MembershipPlan {
-    EMPRESARIAL,            // Categoría A
-    PREMIUM,        // Categoría B
-    NULL        
-}
